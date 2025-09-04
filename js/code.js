48bd35b68a826d5f74d942bf4dfd57a98970e737
Midori <rbk789@protonmail.com>
2025-09-04 15:14:46 -0400

    Fixed bugs

--- js/code.js
+++ js/code.js
@@ -1,8 +1,8 @@
 const urlBase = "http://cop-team16.xyz/API";
-const extension = ".php";
+const extension = "php";
 
 let userID = 0;
-let firstName  "";
+let firstName = "";
 let lastName = "";
 
 function resetDefaults() {
@@ -21,7 +21,7 @@ function doLogin() { // reads username and password
     
     document.getElementById("loginResult").innerHTML = ""; // reset result field
     
-    let login = function(jsonObject) {
+    let loginfunc = function(jsonObject) {
         userID = jsonObject.id;
         
         // if invalid userID returned, login incorrect
@@ -36,12 +36,12 @@ function doLogin() { // reads username and password
         saveCookie();
         
         window.location.href = "contacts.html"; // link to contacts page
-    }
+    };
     
     // if error, print to login result
     let printerr = function(err) {
         document.getElementByID("loginResult").innerHTML = err.message;
-    }
+    };
     
     // call API, call login on result, and printerr if an error happens
     callAPI("Login", { login:login, password:password }, login, printerr);
@@ -55,7 +55,7 @@ function doLogin() { // reads username and password
     let url = urlBase + "/Login." + extension; // construct API url
     
     // create request
-    let xhr = new XMLRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     
@@ -93,23 +93,23 @@ function saveCookie() {
     let date = new Date();
     date.setTime(date.getTime() + (minutes * 60 * 1000));
     
-    document.cookie = "firstName={firstName},lastname={lastName},userID={userID};expires={date.toGMTString()}";
+    document.cookie = 'firstName=${firstName},lastname=${lastName},userID=${userID};expires=${date.toGMTString()}';
 }
 
 // needs API name, parameters as a dictionary, a function taking one string parameter (JSON API output), and a function taking an error
 function callAPI(name, params, func, errfunc) { 
     let jsonPayload = JSON.stringify(params);
     
-    let url = "{urlBase}/{name}.{extension}"; // construct API url
+    let url = '${urlBase}/${name}.${extension}'; // construct API url
     
     // construct request
-    let xhr = new XMLRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     
     try {
-        xhr.onreadystatechange = function {
-            if (this.readystate === 4 && this.status === 200) {
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
                 let jsonObject = JSON.parse(xhr.responseText);
                 
                 func(jsonObject);
