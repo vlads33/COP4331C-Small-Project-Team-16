bae373f394c9b26e2278d4dd996d3e5d1b4d69e5
et195379 <et195379@ucf.edu>
2025-09-18 11:40:38 -0400

    Add files via upload

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,43 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$userId = $inData["userID"];
+	$firstname = $inData["firstName"];
+	$lastname = $inData["lastName"];
+	$email = $inData["email"];
+	$phone = $inData["phone"];
+	$notes = $inData["notes"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserID,FirstName,LastName,Email,Phone,Notes) VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param("ssssss", $userId, $firstname, $lastname, $email, $phone, $notes);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
