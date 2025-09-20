48750605b48621c25939a0fe0db1e756187230c4
et195379 <et195379@ucf.edu>
2025-09-20 15:46:22 -0400

    Updated Add and Delete

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,40 @@
+<?php
+
+	$inData = getRequestInfo();
+	$ID = $inData["ContactID"];
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts where ID=?");
+		$stmt->bind_param("s", $ID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
